bcdf777d77237d3a404292672fb3fe5d5ef63f6e
mysameer <attar.m.sameer@gmail.com>
2022-09-15 01:20:05 +0530

    gh-pages empty

--- frontend/src/components/PageLayout.svelte
+++
@@ -1,3 +0,0 @@
-<div class="container mx-auto px-4 py-4 md:py-6 lg:py-8">
-    <slot />
-</div>
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  content: [
-    './frontend/src/**/*.{svelte,js}',
-    './frontend/public/index.html'
-  ],
-}
